ab72f96098784f8809f6387956e7a28b089df425
kep21 <kep135210@gmail.com>
2021-10-22 17:21:10 +0300

    script

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ alert(arr[1]);
 const result =confirm("Are you here?");
 console.log(result);
 const answer =prompt ("Вам есть 18?","");
-console.log(typeof(answer));*/
+console.log(typeof(answer));
 
 const answers =[];
 
@@ -36,4 +36,44 @@ const Yes = true,
 No = false;
 console.log(Yes || No);
 
-console.log(2 + 2 * 2 !=8);
+console.log(2 + 2 * 2 !=8);*/
+
+
+//создать переменную numberOfFilms и в нее поместить ответ от пользователя на вопрос:"Сколько фильмов ви уже посмотрели?"
+const numberOfFilms =+prompt ("Сколько фильмов ви уже просмотрели?","");
+
+
+
+/*Создать обєкт  personalMovieDB и в него поместить такие свойства:
+- count-сюда передается ответ на первий вопрос
+- movies- в єто свойство поместить пустой обєкт
+- actors- тоже  поместить пустой обєкт
+- genres- сюда поместить пустой массив
+-privat - в єто свойство поместить Boolean(логическое) значение false*/
+ const personalMovieDB = {
+	count: numberOfFilms,
+	movies:{},
+	actors:{},
+	genres:[],
+	privat:false
+ };
+
+
+
+ /*Задайте пользователю по два вопроса:
+ - "Один из последних просмотренних фильмов?"
+ - "На сколько его оцените? "
+ Ответи поместить в отдельние переменние
+ Записать ответи в обьект movies в формате:
+ movies:{
+	 'logan':"8.1"
+ }*/
+
+ const a = prompt("Один из последних просмотренних фильмов?", " "),
+       b = prompt("На сколько оцените его?",""),
+	     c = prompt("Один из последних просмотренних фильмов?", " "),
+       d = prompt("На сколько оцените его?", " ");
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+console.log(personalMovieDB);
