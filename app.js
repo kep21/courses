e27c9514d5b7f5678483d5ab0b346bc302d98618
kep21 <kep135210@gmail.com>
2021-10-22 15:04:14 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+/*const obj = {
+	name:"Serg",
+	age:20,
+	isMarried:false
+};
+console.log(obj.name);
+console.log(obj["age"]);
+
+let arr= ['plum.png', 'orange.jpg', 6,'apple.bmp',{},[]];
+alert(arr[1]);
+
+const result =confirm("Are you here?");
+console.log(result);
+const answer =prompt ("Вам есть 18?","");
+console.log(typeof(answer));*/
+
+const answers =[];
+
+//answers[0] = prompt("как ваше имя?");
+//answers[1] = prompt("сколько вам лет?");
+//answers[2] = prompt("какой ваш пол?");
+
+console.log(typeof(answers));
+
+const category ="toys";
+console.log(`https://someurl.com/${category}/5`);
+
+const user="Krek";
+alert(`HELLO, ${user}`);
+
+const isChecked = true,
+isClose = true;
+console.log(isChecked && isClose);
+
+const Yes = true,
+No = false;
+console.log(Yes || No);
+
+console.log(2 + 2 * 2 !=8);
